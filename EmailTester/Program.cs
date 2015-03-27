498db28ff69a1d11c165fe3a577bb5823be92421
keithf2 <keithfogle@ymail.com>
2015-03-26 20:12:22 -0500

    code
    sending an email with C#

---
+++ EmailTester/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace EmailTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailMessage mail = new MailMessage("foglek@gmail.com", "keithf@qvinci.com");
+            mail.Subject = "Test Email";
+            mail.Body = "Test Email Body";
+            SmtpClient client = new SmtpClient();
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            NetworkCredential cred = new NetworkCredential("foglek@gmail.com", "xxxxxxxx");
+            client.UseDefaultCredentials = false;
+            client.Credentials = cred;
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.Send(mail);
+        }
+    }
+}
